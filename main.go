2117af085de524a10f350cbfec8854c435c36579
nattosTH <natto11113@gmail.com>
2023-10-04 21:18:45 +0700

    -feature :sparkles: add mul

--- main.go
+++ main.go
@@ -6,8 +6,13 @@ func sum(a,b int) int {
 	return a + b
 }
 
+func mul(a,b int) int {
+	return  a *b
+}
+
 func main() {
 	fmt.Println("hello Github")
 
 	fmt.Println(sum(1,2))
+	fmt.Println(mul(1,2))
 }
