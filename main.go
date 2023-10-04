4243ab742eb052cc8b9e86accf24e0f4c717b502
nattosTH <natto11113@gmail.com>
2023-10-04 21:26:47 +0700

    -feature :sparkles: add display

--- main.go
+++ main.go
@@ -10,9 +10,15 @@ func mul(a,b int) int {
 	return  a *b
 }
 
+func display(msg string)  {
+	fmt.Println(msg)
+}
+
 func main() {
 	fmt.Println("hello Github")
 
 	fmt.Println(sum(1,2))
 	fmt.Println(mul(1,2))
+
+	display("hello gitflow")
 }
